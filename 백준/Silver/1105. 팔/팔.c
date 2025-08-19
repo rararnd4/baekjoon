8bc6769c757b7edc0fcb5551db52cc34a4f5f283
rararnd4 <csuhyeok0514@gmail.com>
2025-08-19 15:44:45 +0900

    [Silver I] Title: 팔, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1105. 팔/팔.c
+++ 백준/Silver/1105. 팔/팔.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
-int position(int num){
+int lo(int n){
     int cnt = 0;
-    while(num){
-        num /= 10;
+    while(n){
+        n /= 10;
         cnt++;
     }
     return cnt;
 }
-
 int main(){
     int a, b;
     scanf("%d %d", &a, &b);
-
-    int lim = position(a);
+    int l = lo(a);
     int cnt = 0;
-
-    for(int i = 0; i < lim; i++){
+    for(int i = 0; i < l; i++){
         if(a % 10 == 8){
             if(b - a == 0) cnt++;
         }
         a /= 10;
         b /= 10;
     }
-
     printf("%d", cnt);
 }
